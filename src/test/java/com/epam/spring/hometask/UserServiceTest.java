9e850a53ca2d47b231d36a13040f37c269e5f98a
Denis Semenikhin <Denis_Semenikhin@epam.com>
2018-01-22 02:06:25 +0300

    first Test and succes build

---
+++ src/test/java/com/epam/spring/hometask/UserServiceTest.java
@@ -0,0 +1,44 @@
+package com.epam.spring.hometask;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.epam.spring.hometask.domain.User;
+import com.epam.spring.hometask.service.UserService;
+
+@ContextConfiguration(locations = "classpath:ApplicationContext.xml")
+public class UserServiceTest extends AbstractTestNGSpringContextTests {
+
+	private static final Long ID = 1l;
+	private static final String FIRST_NAME = "Den";
+	private static final String LAST_NAME = "Semenikhin";
+	private static final String EMAIL = "fromrussia@withlove.com";
+
+	@Autowired
+	private UserService userService;
+	private User user;
+
+	@BeforeClass
+	public void initTest() {
+		user = new User();
+		user.setId(ID);
+		user.setFirstName(FIRST_NAME);
+		user.setLastName(LAST_NAME);
+		user.setEmail(EMAIL);
+	}
+
+	@Test(description = "Testing User save() and getById(), remove()")
+	public void userServiceSaveTest() {
+		userService.save(user);
+
+		Assert.assertTrue(userService.getAll().size() == 1);
+		Assert.assertEquals(user, userService.getById(ID));
+		userService.remove(user);
+		Assert.assertTrue(userService.getAll().size() == 0);
+	}
+
+}
